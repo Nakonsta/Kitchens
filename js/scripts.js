8821c4a30e87799662df24e215892778d649da80
Anastasiia Konstantinovich <na.konsta@gmail.com>
2020-07-06 00:03:57 +0300

    Products desktop is almost ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(function(){
     // Плагин для стилизации селектов сортировки и фильтрации на странице каталога
-
     $('.sort__select-js').selectize();
+
+    //Слайдер статей на главной
+    $('.articles__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: $('.articles__prev'),
+        nextArrow: $('.articles__next')
+    })
 });
