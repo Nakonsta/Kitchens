f5f607308b37758f9381af6408c28ad6986e7da1
Anastasiia Konstantinovich <na.konsta@gmail.com>
2020-07-08 00:59:15 +0300

    Catalog adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,26 @@ $(function(){
         slidesToShow: 2,
         slidesToScroll: 2,
         prevArrow: $('.articles__prev'),
-        nextArrow: $('.articles__next')
+        nextArrow: $('.articles__next'),
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     })
+
+    // Анимации
+
+    if($('.catalog').length) {
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > $('.articles').offset().top - 200 ) {
+                $('.articles__item-read-animate').addClass('animated-line');
+            }
+        });
+    }
+
 });
